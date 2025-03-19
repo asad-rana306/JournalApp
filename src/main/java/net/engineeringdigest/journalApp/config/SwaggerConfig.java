62c1320e15c1d27a70e7b4a4dbd23ebe3d69cd6e
Abdul Haseeb <trends.pvtlimited@gmail.com>
2025-03-19 13:40:31 +0500

    add Swagger Tags

--- src/main/java/net/engineeringdigest/journalApp/config/SwaggerConfig.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SwaggerConfig.java
@@ -1,9 +1,15 @@
 package net.engineeringdigest.journalApp.config;
 
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,6 +19,12 @@ public class SwaggerConfig {
         return new OpenAPI().info(
                 new Info().title("Journal App APIs")
                         .description("By Rana Asad")
-        );
+        )
+                .servers(List.of(new Server().url("http://localhost:8081/journal").description("local"),
+                        new Server().url("http://localhost:8082").description("live")))
+                .tags(Arrays.asList(
+                        new Tag().name("Public APIs"),
+                        new Tag().name("User APIs")
+                ));
     }
 }
